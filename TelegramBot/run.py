7ed01782ce7b4858a790b31e8755f7b676807af6
Or Moshe <ormoshe2204@gmail.com>
2021-07-26 23:47:09 +0300

    finish writing telegram registration

--- TelegramBot/run.py
+++ TelegramBot/run.py
@@ -184,7 +184,7 @@ def process_data(user_data):
     data['commander_phone_number'] = user_data.get('commander_phone_number', None)
 
     url = 'http://51.137.47.10:8080/api/register_user/'
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    headers = {'Content-type': 'application/json'}
     res = requests.post(url, data=data, headers=headers)
 
     logger.info(f'status_code: {res.status_code}, {res.content}')
