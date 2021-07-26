1c41bc4c431b463c2928bb114e9da5a8e5c77b9d
Or Moshe <ormoshe2204@gmail.com>
2021-07-26 20:27:03 +0300

    finish writing telegram registration

--- TelegramBot/run.py
+++ TelegramBot/run.py
@@ -17,13 +17,12 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
-                          ConversationHandler, CallbackQueryHandler)
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, \
+    ConversationHandler, CallbackQueryHandler
 
 import logging
 import requests
-import jsonpickle
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -31,77 +30,157 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-CAR, LICENSE_PLATE, PHOTO, DESCRIPTION = range(4)
+CHOOSE, PERSONAL_NUMBER, PHONE_NUMBER, IS_COMMANDER, COMMANDER_PERSONAL_NUMBER, \
+COMMANDER_PHONE_NUMBER, PHOTO, MAC_BLUETOOTH, BLUETOOTH_DEVICE_NAME = range(9)
 
 
 def start(bot, update):
+    reply_keyboard = [['Register', 'Delete']]
+
+    update.message.reply_text(
+        'Hi! Welcome to Smadi-ML :)\n'
+        'Pleace choose an option: Register or Update',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return CHOOSE
+
+
+def choose(bot, update, user_data):
+    user = update.message.from_user
+    logger.info(f'choose: {user.first_name}: {update.message.text}')
+
+    if update.message.text == 'Register':
+        update.message.reply_text('OK! Let\'s start the registration process,\n'
+                                  'Please fill in your personal number',
+                                  reply_markup=ReplyKeyboardRemove())
+        return PERSONAL_NUMBER
+    else:
+        # TODO: fill
+        update.message.reply_text('Bye! I hope we can talk again some day.',
+                                  reply_markup=ReplyKeyboardRemove())
+
+        return ConversationHandler.END
+        # return Delete
+
+
+def personal_number(bot, update, user_data):
+    user = update.message.from_user
+    logger.info(f'personal_number: {user.first_name}: {update.message.text}')
+
+    user_data['personal_number'] = update.message.text
+    update.message.reply_text(
+        'Thank You. Now, please click to send your phone number',
+        reply_markup=KeyboardButton('Click to send the phone number', request_contact=True))
+
+    return PHONE_NUMBER
+
+
+def phone_number(bot, update, user_data):
+    contact = update.effective_message.contact
+    user_data['phone_number'] = contact.phone_number
+
+    user = update.message.from_user
+    logger.info(f'phone_number: {user.first_name}: {contact.phone_number}')
+
     reply_keyboard = [['Yes', 'No']]
 
     update.message.reply_text(
-        'Hi! I\'m from the future and i\'m in charge of entrance approvals. I will hold a conversation with you. '
-        'Send /cancel to stop talking to me.\n\n'
-        'Do you want to enter with a car?',
+        'Thank You. Do you want to enter your commander\'s details?',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
-    return CAR
+    return IS_COMMANDER
 
 
-def licence_plate(bot, update, user_data):
-    user_data['plate'] = update.message.text
-    update.message.reply_text(
-        'Thank You. Now, please send me a photo of your ID',
-        reply_markup=ReplyKeyboardRemove())
-    return PHOTO
-
-
-def car(bot, update, user_data):
+def is_commander(bot, update, user_data):
     user = update.message.from_user
-    user_data['car'] = update.message.text
-    logger.info("%s: enters with a car? %s", user.first_name, update.message.text)
+    logger.info(f'is_commander: {user.first_name}: {update.message.text}')
+
     if update.message.text == 'Yes':
-        update.message.reply_text('I see! Please send me your license plate number.',
+        update.message.reply_text('Please fill in your commander\'s personal number',
                                   reply_markup=ReplyKeyboardRemove())
-        return LICENSE_PLATE
+        return COMMANDER_PERSONAL_NUMBER
     else:
-        update.message.reply_text('I see! Please send me a photo of your ID',
+        update.message.reply_text('I see! Please send me a photo of you\n'
+                                  'Please make sure that the photo is in high quality!',
                                   reply_markup=ReplyKeyboardRemove())
         return PHOTO
 
 
+def commander_personal_number(bot, update, user_data):
+    user = update.message.from_user
+    logger.info(f'commander_personal_number: {user.first_name}: {update.message.text}')
+
+    user_data['commander_personal_number'] = update.message.text
+
+    update.message.reply_text('Please fill in your commander\'s phone number',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return COMMANDER_PHONE_NUMBER
+
+
+def commander_phone_number(bot, update, user_data):
+    user = update.message.from_user
+    logger.info(f'commander_phone_number: {user.first_name}: {update.message.text}')
+
+    user_data['commander_phone_number'] = update.message.text
+
+    reply_keyboard = [['Yes', 'No']]
+
+    update.message.reply_text('I see! Please send me a photo of you\n'
+                              'Please make sure that the photo is in high quality!',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return PHOTO
+
+
 def photo(bot, update, user_data):
     user = update.message.from_user
+    logger.info(f'photo: {user.first_name} uploaded a photo')
+
     photo_file = bot.get_file(update.message.photo[-1].file_id)
-    user_data['photo'] = photo_file
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
-    update.message.reply_text('Gorgeous! What is the reason for entrance?')
+    user_data['photo'] = photo_file.download_as_bytearray()
 
-    return DESCRIPTION
+    update.message.reply_text('Gorgeous! Please enter your MAC Bluetooth',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return MAC_BLUETOOTH
 
 
-def description(bot, update, user_data):
+def mac_bluetooth(bot, update, user_data):
     user = update.message.from_user
-    user_data['description'] = update.message.text
-    user_data['user'] = user
-    user_data['chat_id'] = update.message.chat_id
-    logger.info("Description of %s: %s", user.first_name, update.message.text)
+    logger.info(f'mac_bluetooth: {user.first_name}: {update.message.text}')
+
+    user_data['mac_bluetooth'] = update.message.text
+    update.message.reply_text('Please enter your Bluetooth device name',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return BLUETOOTH_DEVICE_NAME
+
+
+def bluetooth_device_name(bot, update, user_data):
+    user = update.message.from_user
+    logger.info(f'bluetooth_device_name: {user.first_name}: {update.message.text}')
+
+    user_data['bluetooth_device_name'] = update.message.text
+    update.message.reply_text('Please enter your Bluetooth device name')
+
     process_data(user_data)
 
-    update.message.reply_text('Thank you! Your entrance request was processed and sent for appproval')
+    update.message.reply_text('Thank you! Your registration request was processed')
+
     return ConversationHandler.END
 
 
-def process_data(user_data):
-    body = {
-        'photo': user_data['photo'].download_as_bytearray(),
-        'user': user_data['user'],
-        'description': user_data['description'],
-        'plate': user_data['plate'] if 'plate' in user_data else None,
-        'chat_id': user_data['chat_id']
-    }
 
-    url = 'http://10.104.236.26:8080/api/AddEntryPermit'
-    requests.post(url, data=jsonpickle.encode(body))
-    return body
+def process_data(user_data):
+    'register_user/<str:personal_number>/<str:commander_id>/<str:phone_number>/<str:mac_bluetooth>/<str:device_name>/'
+    user_data['commander_personal_number'] = user_data.get('commander_personal_number', None)
+    user_data['commander_phone_number'] = user_data.get('commander_phone_number', None)
+
+    url = 'http://10.104.236.26:8080/api/register_user'
+    res = requests.post(url, data=user_data)
+
+    logger.info(f'status_code: {res.status_code}')
 
 
 def cancel(bot, update):
@@ -128,11 +207,17 @@ def main():
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
+
         states={
-            CAR: [RegexHandler('^(Yes|No)$', car, pass_user_data=True)],
-            LICENSE_PLATE: [RegexHandler('^[0-9]{7,8}$', licence_plate, pass_user_data=True)],
+            CHOOSE: [RegexHandler('^(Register|Delete)$', choose, pass_user_data=True)],
+            PERSONAL_NUMBER: [RegexHandler('^[0-9]{7}$', personal_number, pass_user_data=True)],
+            PHONE_NUMBER: [MessageHandler(Filters.contact, phone_number, pass_user_data=True)],
+            IS_COMMANDER: [RegexHandler('^(Yes|No)$', is_commander, pass_user_data=True)],
+            COMMANDER_PERSONAL_NUMBER: [RegexHandler('^[0-9]{7}$', commander_personal_number, pass_user_data=True)],
+            COMMANDER_PHONE_NUMBER: [RegexHandler('^[0-9]{10}$', commander_phone_number, pass_user_data=True)],
             PHOTO: [MessageHandler(Filters.photo, photo, pass_user_data=True)],
-            DESCRIPTION: [MessageHandler(Filters.text, description, pass_user_data=True)],
+            MAC_BLUETOOTH: [MessageHandler(Filters.text, mac_bluetooth, pass_user_data=True)],
+            BLUETOOTH_DEVICE_NAME: [MessageHandler(Filters.text, bluetooth_device_name, pass_user_data=True)],
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
